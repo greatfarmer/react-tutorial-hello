efaad2528f296eb9f9819e9f4adeef77ddfe0d87
Seonghun <lutis107@gmail.com>
2018-08-15 22:25:48 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React-tutorial-hello
+[![author](https://img.shields.io/badge/author-greatfarmer-f28da5.svg)](https://github.com/greatfarmer)
+
+Study React!
+
+## Progress
+- Chapter 01~03 | 2018-08-13
+- Chapter 04~06 | 2018-08-14
+
+## Reference
+- [리액트를 다루는 기술](http://www.gilbut.co.kr/book/bookView.aspx?bookcode=BN002044&page=1&TF=T), 김민준, 길벗 (2018)
---
+++ src/IterationSample.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class IterationSample extends Component {
+  state = {
+    names: ['눈사람', '얼음', '눈', '바람'],
+    name: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleInsert = () => {
+    this.setState({
+      names: this.state.names.concat(this.state.name),
+      name: ''
+    })
+    this.input.focus();
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleInsert();
+    }
+  }
+
+  handleRemove = (index) => {
+    const { names } = this.state;
+
+    this.setState({
+      names: names.filter((item, i) => i !== index)
+    });
+  }
+
+  render() {
+    const nameList = this.state.names.map(
+      (name, index) => (
+        <li
+          key={index}
+          onDoubleClick={() => this.handleRemove(index)}
+        >
+          {name}
+        </li>
+      )
+    );
+
+    return (
+      <div>
+        <input
+          ref={(ref) => this.input=ref}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+          value={this.state.name}
+        />
+        <button onClick={this.handleInsert}>추가</button>
+        <ul>
+          {nameList}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default IterationSample;
